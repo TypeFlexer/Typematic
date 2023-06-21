0e4bb2febff5c85f68eda0a349eca378845d03fc
arun <bhattar1@purdue.edu>
2023-06-21 14:54:20 -0400

    Commit - 1 - Regression OK - Basic Changes

--- include/stdlib_tainted.h
+++ include/stdlib_tainted.h
@@ -17,7 +17,7 @@
 #include <stddef.h>
 
 // TODO: express alignment constraints once where clauses have been added.
-#ifdef WASM_SBX
+//#ifdef WASM_SBX
 _Itype_for_any(T) _TArray_ptr<T> t_malloc(size_t size);
 
 _Itype_for_any(T) void t_free(_TPtr<T> pointer);
@@ -25,7 +25,7 @@ _Itype_for_any(T) void t_free(_TPtr<T> pointer);
 _Itype_for_any(T) _TArray_ptr<T> t_realloc(_TPtr<T> pointer, size_t size);
 
 _Itype_for_any(T) _TArray_ptr<T> t_calloc(size_t nmemb, size_t size);
-#elif HEAP_SBX
+//#elif HEAP_SBX
 
 extern _Itype_for_any(T) _TPtr<T> hoard_malloc(size_t size);
 extern _Itype_for_any(T) void hoard_free(_TPtr<T> ptr);
@@ -36,7 +36,7 @@ extern int isPointerinHeap(void *p);
 extern int CacheUpdateandCheck(void *p);
 extern _Tainted void registerCallback(_TPtr<void> callbackFunc);
 extern _Tainted void unregisterCallback(_TPtr<void> callbackFunc);
-#endif
+//#endif
 
 /* Convert a string to a floating-point number.  */
 extern double t_strtod (_TPtr<char> __restrict __nptr,
