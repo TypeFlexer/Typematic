b4e43759aa99a8db1b13e6784e27adea3e6e30d5
arun <bhattar1@purdue.edu>
2023-09-22 02:08:58 -0400

    Merge remote-tracking branch 'origin/typeflexer' into typeflexer Merge pull request #2 from TypeFlexer/TypeFlexer-typeflexer_merge
    Type flexer typeflexer merge
    Commit - 1 - Regression OK - Basic Changes

--- include/stdlib_tainted.h
+++ include/stdlib_tainted.h
@@ -16,8 +16,6 @@
 
 #include <stddef.h>
 
-// TODO: express alignment constraints once where clauses have been added.
-//#ifdef WASM_SBX
 _Itype_for_any(T) _TArray_ptr<T> t_malloc(size_t size);
 
 _Itype_for_any(T) void t_free(_TPtr<T> pointer);
@@ -25,7 +23,6 @@ _Itype_for_any(T) void t_free(_TPtr<T> pointer);
 _Itype_for_any(T) _TArray_ptr<T> t_realloc(_TPtr<T> pointer, size_t size);
 
 _Itype_for_any(T) _TArray_ptr<T> t_calloc(size_t nmemb, size_t size);
-//#elif HEAP_SBX
 
 extern _Itype_for_any(T) _TPtr<T> hoard_malloc(size_t size);
 extern _Itype_for_any(T) void hoard_free(_TPtr<T> ptr);
@@ -36,7 +33,6 @@ extern int isPointerinHeap(void *p);
 extern int CacheUpdateandCheck(void *p);
 extern _Tainted void registerCallback(_TPtr<void> callbackFunc);
 extern _Tainted void unregisterCallback(_TPtr<void> callbackFunc);
-//#endif
 
 /* Convert a string to a floating-point number.  */
 extern double t_strtod (_TPtr<char> __restrict __nptr,
