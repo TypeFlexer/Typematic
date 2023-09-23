25ad0e106f1e7e78135e0cd270611315b267d649
arun <bhattar1@purdue.edu>
2023-09-23 02:40:50 -0400

    minor changes

--- typematic-tests/output/nsimple_expected.c
+++ typematic-tests/output/nsimple_expected.c
@@ -3,12 +3,12 @@
 #include <stdlib_tainted.h>
 
 _Tainted _TPtr<int> increment(_TPtr<int> tainted_ptr) {
-    (*tainted_ptr)++;
-    return tainted_ptr;
+(*tainted_ptr)++;
+return tainted_ptr;
 }
 
 _Tainted int main() {
-    _TPtr<int> seed_tainted_ptr = t_malloc(sizeof(int));
+    _TPtr<int> seed_tainted_ptr = t_malloc<int>(sizeof(int));
     *seed_tainted_ptr = 10;
 
     printf("Value before increment: %d\n", *seed_tainted_ptr);
